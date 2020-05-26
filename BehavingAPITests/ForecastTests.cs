58a0a1859503d1ceaae2a8ea3616ee1db480d16b
Lisandro <lisandroalavez@gmail.com>
2020-05-26 16:08:12 +0100

    Initial commit with tests

---
+++ BehavingAPITests/ForecastTests.cs
@@ -0,0 +1,32 @@
+using BehavingAPI;
+using BehavingAPI.Controllers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BehavingAPITests
+{
+    public class ForecastTests
+    {
+        WeatherForecastController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new WeatherForecastController(null);
+        }
+
+        [Test]
+        public void Forecast_Should_be_valid_next_5_days()
+        {
+            // act
+            var weatherForecasts = _controller.Get();
+
+            // assert
+            //Assert.AreEqual(5, weatherForecasts.ToList().Count);
+            weatherForecasts.Should()
+                .NotBeEmpty()
+                .And.HaveCount(5)
+                .And.ContainItemsAssignableTo<WeatherForecast>();
+        }
+    }
+}
